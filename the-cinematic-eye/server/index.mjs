117d76fcdb3f9a0f7c2fbeb5ef0a7644deb5ae2b
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-11-29 13:27:59 +0100

    Module fix Installed -Morgan -Cors Added dev script frontend and backend

---
+++ the-cinematic-eye/server/index.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const port = 3000;
+
+const app = express();
+
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_req, res) => {
+  res.send({ message: "Hello, world!" });
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});
