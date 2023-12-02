769d217324d1c39056fafeddec3b5707dff0bd4b
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-12-02 16:59:49 +0100

    Hashed in argon2 password

--- the-cinematic-eye/server/controllers/Authentication.mjs
+++ the-cinematic-eye/server/controllers/Authentication.mjs
@@ -1,15 +1,16 @@
+import argon2d from "argon2";
 import {users} from "../models/index.mjs"
 export default {
     async register(req, res){
-        try {
+
             console.log(req.body);
-            const user = await users.create(req.body);
+            const bodyHashed = {
+                email: req.body.email,
+                password: await argon2d.hash(req.body.password)
+            }
+            const user = await users.create(bodyHashed);
             res.send(user.toJSON())
-        } catch (error) {
-            res.status(400).send({
-                e: error,
-                b: req.body
-            })
-        }
+        
     }
+
 }
