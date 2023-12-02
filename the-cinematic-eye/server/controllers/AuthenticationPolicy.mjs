69628e1aff64ab8ff4224d62d862d5b39c43964c
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-12-03 00:09:10 +0100

    Created AuthorizationPolicy regex controllof  data

---
+++ the-cinematic-eye/server/controllers/AuthenticationPolicy.mjs
@@ -0,0 +1,32 @@
+import Joi from "joi";
+
+export default{ 
+    register(req, res, next){
+        const schema = Joi.object({
+            email: Joi.string().email(),
+            password: Joi.string().regex(
+                new RegExp('^[a-zA-Z0-9]{8,32}$')
+            )
+        });
+
+        const {error, value} = schema.validate(req.body);
+        if (error) {
+            switch(error.details[0].context.key){
+                case 'email':
+                    res.status(400).send({
+                        error: 'Invalid email'})
+                    break
+                case 'password':
+                    res.status(400).send({
+                        error: 'Invalid password <br> Must contain:<br> a lowercase letter<br>a uppercase letter<br>a number<br>at least 8 characters<br>at maximum 32 characters'})
+                    break
+                default:
+                    res.status(400).send({
+                        error: 'Invalid registration info'})
+            }
+        } else {
+           next(); 
+        }
+        
+    }
+}
