65845f0e5b94677de10c3d3e552afac5a09e2b02
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-12-03 00:10:16 +0100

    Added login and policy to backend routes

--- the-cinematic-eye/server/routes.mjs
+++ the-cinematic-eye/server/routes.mjs
@@ -1,11 +1,12 @@
 import Authentication from "./controllers/Authentication.mjs";
 
-
+import AuthenticationPolicy from "./controllers/AuthenticationPolicy.mjs";
 
 export default function(app) {
     app.get("/", (_req, res) => {
         res.send({ message: "Hello there!" });
       });
       
-    app.post("/register", Authentication.register);
+    app.post("/register", AuthenticationPolicy.register, Authentication.register);
+    app.post("/login", AuthenticationPolicy.register,Authentication.login);
 }
