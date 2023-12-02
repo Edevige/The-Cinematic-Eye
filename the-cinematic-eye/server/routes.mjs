815ba9994ddf8f26c3ca1800358b1514b72902e1
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-12-02 12:14:11 +0100

    Added registation route in server routes

--- the-cinematic-eye/server/routes.mjs
+++ the-cinematic-eye/server/routes.mjs
@@ -1,6 +1,11 @@
+import Authentication from "./controllers/Authentication.mjs";
+
+
+
 export default function(app) {
     app.get("/", (_req, res) => {
         res.send({ message: "Hello there!" });
       });
-
+      
+    app.post("/register", Authentication.register);
 }
