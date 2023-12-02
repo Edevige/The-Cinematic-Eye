a8abcc49e5f599df35107921f0c82007449c058a
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-12-03 00:11:19 +0100

    Added login and trycatch to auth controller

--- the-cinematic-eye/server/controllers/Authentication.mjs
+++ the-cinematic-eye/server/controllers/Authentication.mjs
@@ -1,16 +1,34 @@
-import argon2d from "argon2";
+import argon2id from "argon2";
 import {users} from "../models/index.mjs"
 export default {
     async register(req, res){
-
-            console.log(req.body);
             const bodyHashed = {
                 email: req.body.email,
-                password: await argon2d.hash(req.body.password)
+                password: await argon2id.hash(req.body.password)
             }
+        try {
             const user = await users.create(bodyHashed);
-            res.send(user.toJSON())
-        
+            res.send(user.toJSON()) 
+        } catch (error) { 
+            res.status(400).send({er: error})
+        }         
+    },
+    async login(req, res){
+        try {
+            const match = await users.findAll({
+                where: {
+                  email: req.body.email
+                }
+              });
+            if (await argon2id.verify(match[0].password, req.body.password)) {
+                res.send({ message: "General "+req.body.email });
+            } else {
+                res.send({ message: "NOP!" });
+            }
+          } catch (err) {
+            res.send({
+                er: err
+            })
+          }
     }
-
 }
