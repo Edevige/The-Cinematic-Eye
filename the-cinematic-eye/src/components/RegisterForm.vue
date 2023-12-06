54d78de4cac16d38dc85e8d0769f01a22d367acd
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-12-06 18:59:38 +0100

    Format

--- the-cinematic-eye/src/components/RegisterForm.vue
+++ the-cinematic-eye/src/components/RegisterForm.vue
@@ -1,49 +1,38 @@
 <template>
-    <div>
-        <form 
-          name="register-form"
-          autocomplete="off">
-          <div class="mb-3">
-          <label class="form-label" for="emailInput">Email</label>
-          <input type="email"
-            class="form-control text-center"
-            id="emailInput"
-            placeholder="email"
-            v-model="email">
-          </div>
-          <div class="mb-3">
-          <label class="form-label" for="passInput">Password</label>
-          <input
-            class="form-control text-center"
-            id="passInput"
-            type="password"
-            placeholder="password"
-            v-model="password"
-            autocomplete="new-password"
-          >
-        </div>
-        </form>
-        <br>
-        <div v-if="error !== null " class="alert alert-danger alert-dismissible ms-5 me-5" role="alert" >{{ error }}<button type="button" class="btn-close" @click="error=null;" aria-label="Close"></button></div>
-        <br>
-        <button type="button" class="btn btn-light" @click="register" >Register
-        </button>
-    </div>
+  <div>
+    <form name="register-form" autocomplete="off">
+      <div class="mb-3">
+        <label class="form-label" for="emailInput">Email</label>
+        <input type="email" class="form-control text-center" id="emailInput" placeholder="email" v-model="email">
+      </div>
+      <div class="mb-3">
+        <label class="form-label" for="passInput">Password</label>
+        <input class="form-control text-center" id="passInput" type="password" placeholder="password" v-model="password"
+          autocomplete="new-password">
+      </div>
+    </form>
+    <br>
+    <div v-if="error !== null" class="alert alert-danger alert-dismissible ms-5 me-5" role="alert">{{ error }}<button
+        type="button" class="btn-close" @click="error = null;" aria-label="Close"></button></div>
+    <br>
+    <button type="button" class="btn btn-light" @click="register">Register
+    </button>
+  </div>
 </template>
 
 <script>
 import AuthenticationService from '../services/AuthenticationService.js'
 
 export default {
-    data(){
-        return{
-            email: '',
-            password: '',
-            error: null
-        }
-    },
-    methods:{
-        async register () {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async register() {
       try {
         const response = await AuthenticationService.register({
           email: this.email,
@@ -56,17 +45,18 @@ export default {
         this.error = error.response.data.error
       }
     }
-    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-    .alert{
-      font-size: large;
-      font-weight: bold;
-    }
-    .form-label{
-      font-size: x-large;
-      color:whitesmoke;
-    }
+.alert {
+  font-size: large;
+  font-weight: bold;
+}
+
+.form-label {
+  font-size: x-large;
+  color: whitesmoke;
+}
 </style>
