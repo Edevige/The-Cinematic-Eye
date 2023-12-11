5822bca644fef3c92ab615f5b5db27f57aecd414
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2023-12-11 17:22:19 +0100

    Added base info to User model and policy

--- the-cinematic-eye/server/controllers/AuthenticationPolicy.mjs
+++ the-cinematic-eye/server/controllers/AuthenticationPolicy.mjs
@@ -1,32 +1,35 @@
 import Joi from "joi";
 
-export default{ 
-    register(req, res, next){
-        const schema = Joi.object({
-            email: Joi.string().email(),
-            password: Joi.string().regex(
-                new RegExp('^[a-zA-Z0-9]{8,32}$')
-            )
-        });
+export default {
+  register(req, res, next) {
+    const schema = Joi.object({
+      email: Joi.string().email(),
+      password: Joi.string().regex(new RegExp("^[a-zA-Z0-9]{8,32}$")),
+      username: Joi.string().regex(new RegExp("^[a-zA-Z0-9]{2,16}$")),
+      name: Joi.string().regex(new RegExp("^(?![s.]+$)[a-zA-Zs.]*$")),
+    });
 
-        const {error, value} = schema.validate(req.body);
-        if (error) {
-            switch(error.details[0].context.key){
-                case 'email':
-                    res.status(400).send({
-                        error: 'Invalid email'})
-                    break
-                case 'password':
-                    res.status(400).send({
-                        error: 'Invalid password <br> Must contain:<br> a lowercase letter<br>a uppercase letter<br>a number<br>at least 8 characters<br>at maximum 32 characters'})
-                    break
-                default:
-                    res.status(400).send({
-                        error: 'Invalid registration info'})
-            }
-        } else {
-           next(); 
-        }
-        
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      switch (error.details[0].context.key) {
+        case "email":
+          res.status(400).send({
+            error: "Invalid email",
+          });
+          break;
+        case "password":
+          res.status(400).send({
+            error:
+              "Invalid password <br> Must contain:<br> a lowercase letter<br>a uppercase letter<br>a number<br>at least 8 characters<br>at maximum 32 characters",
+          });
+          break;
+        default:
+          res.status(400).send({
+            error: "Invalid registration info",
+          });
+      }
+    } else {
+      next();
     }
-}
+  },
+};
--- the-cinematic-eye/server/models/Users.mjs
+++ the-cinematic-eye/server/models/Users.mjs
@@ -1,46 +1,59 @@
 import argon2id from "argon2";
 
-async function hashPass(user){
-    if(!user.changed('password')){
-        return
-    }
-    try {
-         const passHash = await argon2id.hash(user.password);
-         user.setDataValue('password',passHash);
-         return passHash
-    } catch (error) {
-        console.log('error',error);
-    }
-    
+async function hashPass(user) {
+  if (!user.changed("password")) {
+    return;
+  }
+  try {
+    const passHash = await argon2id.hash(user.password);
+    user.setDataValue("password", passHash);
+    return passHash;
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
-export default function(sequelize, DataTypes){
-    const user = sequelize.define('Users', {
-        email:{
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false
-        },
-        password:{
-            type: DataTypes.STRING,
-            allowNull: false 
-        }
-    },{hooks: {
+export default function (sequelize, DataTypes) {
+  const user = sequelize.define(
+    "Users",
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      birthdate: {
+        type: DataTypes.DATEONLY,
+      },
+    },
+    {
+      hooks: {
         beforeUpdate: hashPass,
-        beforeSave: hashPass
-    }})
-
-
-    user.prototype.comparePass = async function (password){
-        try {
-             const isEqual = await argon2id.verify(this.password, password);
-             return isEqual;
-        } catch (error) {
-            console.log(error);
-        }
-        
+        beforeSave: hashPass,
+      },
     }
+  );
 
-    return user;
+  user.prototype.comparePass = async function (password) {
+    try {
+      const isEqual = await argon2id.verify(this.password, password);
+      return isEqual;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  return user;
 }
