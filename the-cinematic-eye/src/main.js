bfd4abbe5d36e865075bb3650728b5f42d2a9154
Mich2202 <michael.scicchitano01@gmail.com>
2024-02-29 14:58:44 +0100

    Import GoogleLogin

--- the-cinematic-eye/src/main.js
+++ the-cinematic-eye/src/main.js
@@ -6,5 +6,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import store from './services/store';
+import vue3GoogleLogin from 'vue3-google-login'
 
-createApp(App).use(router).use(store).use().mount('#app');
+createApp(App).use(router).use(store).use(vue3GoogleLogin, {
+    clientId: '599203859511-bf0n3k9qptlmd61n2butlap8s39gsrgb.apps.googleusercontent.com'
+}).mount('#app');
