4bf514e4235f9e63b136856c443344125d2a76ff
Mich2202 <michael.scicchitano01@gmail.com>
2024-03-12 16:49:26 +0100

    Added facebookAppId to Config

--- the-cinematic-eye/server/config/config.mjs
+++ the-cinematic-eye/server/config/config.mjs
@@ -14,6 +14,7 @@ const config = {
     authentication: {
        jwtSecret: process.env.JWT_SECRET || 'secret'
     },
-    googleClientId:'599203859511-5f3c2e9dkgg7qjplu44f4qa1i57t1kf9.apps.googleusercontent.com'
+    googleClientId:'599203859511-5f3c2e9dkgg7qjplu44f4qa1i57t1kf9.apps.googleusercontent.com',
+    facebookAppId:'1079656656483027'
 };
 export default config
