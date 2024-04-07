952a2309cbdf9873a79d2e988d776a24026a8b43
Mich2202 <michael.scicchitano01@gmail.com>
2024-04-07 15:22:22 +0200

    Started the Action Film category

--- the-cinematic-eye/src/views/ActionView.vue
+++ the-cinematic-eye/src/views/ActionView.vue
@@ -1,5 +1,29 @@
+<script setup>
+
+import FilmCarousel from '@/components/FilmCarousel.vue';
+</script>
+
 <template>
-    <div class="about">
-      <h1>This is the Action page... BOOM!</h1>
+  <main>
+    <div class="wrapper">
+      <FilmCarousel :prop-url="'discover/movie'" title="Azione"/>
     </div>
-  </template>
+
+  </main>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+  
+  main{
+    display: flex;
+    place-items: center;
+  }
+  main .wrapper {
+    display: flex;
+    place-items: center;
+    flex-wrap: wrap;
+  }
+}
+</style>
