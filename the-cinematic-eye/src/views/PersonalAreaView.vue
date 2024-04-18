7fa02f3d99a116efb235fc297ea67edce291df83
Mich2202 <michael.scicchitano01@gmail.com>
2024-04-18 16:18:16 +0200

    Updates to personal area

--- the-cinematic-eye/src/views/PersonalAreaView.vue
+++ the-cinematic-eye/src/views/PersonalAreaView.vue
@@ -1,5 +1,25 @@
 <template>
-    <div class="about">
-      <h1>This is the personal Area</h1>
-    </div>
-  </template>
+  <div class="personal-area">
+    <h1>Area Personale</h1>
+      <label for="name">Username: {{ this.$store.state.user.username }}</label>
+      <div></div>
+      <label for="email">Email: {{ this.$store.state.user.email }}</label>
+      <div></div>
+      <button type="button" @click="logout()">Logout</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+   methods:{
+    logout(){
+            this.user = {};
+            this.jwt = "";
+            this.$store.commit('logout');
+            this.$router.push("/")
+        }
+   }
+}
+
+</script>
