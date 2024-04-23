ebfd9322516270517e06c6bd00a8c8890feb2ff8
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2024-04-23 19:38:18 +0200

    added debug view for the store

--- the-cinematic-eye/src/views/DebugStoreView.vue
+++ the-cinematic-eye/src/views/DebugStoreView.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="about">
+    <div class="debug">
       <h1>Store</h1>
       <h2>{{ this.$store.state }}</h2>
     </div>
   </template>
-  
+  <style lang="scss" scoped>
+  .debug {
+    color: whitesmoke
+}</style>
