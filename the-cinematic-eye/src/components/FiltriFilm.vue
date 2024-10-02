4fa7fbcae5f9de45e1380805fd72584e26175f5f
Mich2202 <michael.scicchitano01@gmail.com>
2024-10-02 16:28:19 +0200

    Aggiunta Pagina per filtrare i Film

---
+++ the-cinematic-eye/src/components/FiltriFilm.vue
@@ -0,0 +1,10 @@
+<template>
+        <!-- Striscia orizzontale con gli strumenti per filtrare i Film in varie pagine (dove è necessario) -->
+        <div class="d-flex justify-content-around bg-secondary text-white p-3">
+            <div class="p-2">Elemento 1</div>
+            <div class="p-2">Elemento 2</div>
+            <div class="p-2">Elemento 3</div>
+            <div class="p-2">Elemento 4</div>
+        </div>
+
+</template>
