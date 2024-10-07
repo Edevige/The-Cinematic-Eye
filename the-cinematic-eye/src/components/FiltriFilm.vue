4bd5e668ae2d807ad284f2c27e7713a1065ec6ac
Mich2202 <michael.scicchitano01@gmail.com>
2024-10-07 23:48:31 +0200

    Update FiltriFilm.vue

--- the-cinematic-eye/src/components/FiltriFilm.vue
+++ the-cinematic-eye/src/components/FiltriFilm.vue
@@ -1,10 +1,12 @@
 <template>
         <!-- Striscia orizzontale con gli strumenti per filtrare i Film in varie pagine (dove è necessario) -->
         <div class="d-flex justify-content-around bg-secondary text-white p-3">
-            <div class="p-2">Elemento 1</div>
-            <div class="p-2">Elemento 2</div>
-            <div class="p-2">Elemento 3</div>
-            <div class="p-2">Elemento 4</div>
+            <div class="p-2">Genere</div>
+            <div class="p-2">Anno di Uscita</div>
+            <div class="p-2">Durata</div>
+            <div class="p-2">Regista</div>
+            <div class="p-2">Cast</div>
+            <div class="p-2">Valutazione</div>
         </div>
 
 </template>
