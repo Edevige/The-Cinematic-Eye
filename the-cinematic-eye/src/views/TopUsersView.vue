50a6f439b36abc288f22ca157be43c108f3e7e11
Mich2202 <michael.scicchitano01@gmail.com>
2024-10-08 12:55:12 +0200

    Create pagine per i Top USer e i profili per gli utenti singoli

---
+++ the-cinematic-eye/src/views/TopUsersView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="d-flex flex-column">
+      <div v-for="item in userFound">
+          <router-link class="d-flex flex-row" :to="'/user/'+item.id" style="appearance: none !important; text-decoration: none;">
+          <div class="col d-flex align-items-center " style="color:white"><div class="h4 m-4">{{ item.username }}</div></div>
+          </router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+
+import Users from '../../server/models/Users.mjs'
+
+export default{
+
+  data(){
+    return{
+      userFound:[]
+    }
+  },
+  /*methods: {
+    async fetchUser(){
+      try {
+        const response= await Users.findAll();
+        this.userFound=response.data;
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+  mounted(){
+    this.fetchUser();
+  }*/
+}
+
+</script>
