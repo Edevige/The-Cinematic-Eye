68c3f813de2580e3ee80b993b92da66a561bd91e
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2024-10-12 17:18:40 +0200

    Created loadingSpinner component for readability

---
+++ the-cinematic-eye/src/components/LoadingSpinner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="position-relative ">
+          <div class=" row w-100 mx-auto">
+            <div>
+              <div class="mt-4 spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
