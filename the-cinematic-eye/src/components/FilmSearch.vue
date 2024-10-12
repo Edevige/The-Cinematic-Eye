18a6f6c163f463abf4e585614f52f6c423b6ff4a
Edvige <tavoletti.1956733@studenti.uniroma1.it>
2024-10-12 17:20:02 +0200

    Added loading spinner to FilmSearch Component

--- the-cinematic-eye/src/components/FilmSearch.vue
+++ the-cinematic-eye/src/components/FilmSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="d-flex flex-column">
-        <div  v-for="item in filmFound">
+        <div v-if="filmFound > 0" v-for="item in filmFound">
             <router-link class="d-flex flex-row" :to="'/film/'+item.id" style="appearance: none !important; text-decoration: none;">
             <div class="col-1">
                 <img v-if="item.poster_path != null" class="img img-fluid w-auto mt-4" :src="imgUrl + item.poster_path" />
@@ -11,13 +11,18 @@
             <div class="col d-flex align-items-center " style="color:white"><div class="h4 m-4">{{ item.original_title }}</div></div>
             </router-link>
         </div>
+        <LoadingSpinner v-else/>
     </div>
 </template>
 
 <script>
 import TMdbApi from '@/services/TMdbApi';
+import LoadingSpinner from './LoadingSpinner.vue';
 
 export default {
+    components:{
+        LoadingSpinner
+    },
     props:{
         searchParameter: String
     },
@@ -56,7 +61,7 @@ export default {
             return 0;
         }
     },
-    mounted() {
+    created() {
         this.searchFilm(this.searchParameter);
     }
 }
