a0fdd33c3f6b044b0799d6611909acb9c4494b77
Mich2202 <michael.scicchitano01@gmail.com>
2024-10-17 13:33:16 +0200

    Salvataggio (Temporaneo) per mailchimp

--- the-cinematic-eye/src/services/MailChimpApi.js
+++ the-cinematic-eye/src/services/MailChimpApi.js
@@ -1,12 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
-export default () => {
-    return axios.create({
-        baseURL: "https://us22.api.mailchimp.com/3.0/",
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer 109a471a8a84665a48ae7625fe15233a-us22'
-        }
-        
-    })
-}
+const MailChimpApi = axios.create({
+    baseURL: "https://us22.api.mailchimp.com/3.0/", 
+    headers: {
+        accept: 'application/json',
+        Authorization: `Basic ${btoa('anystring:109a471a8a84665a48ae7625fe15233a-us22')}`
+    }
+});
+
+export default MailChimpApi;
